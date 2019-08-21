c5d4234d535d36d67d96df129685725bedcd2e62
fgutierrezz <30966360+fgutierrezz@users.noreply.github.com>
2019-08-21 10:16:07 -0400

    Create enable_all_jobs_scheduled.sh

---
+++ enable_all_jobs_scheduled.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Enable All scheduled jobs 
+# Author: Felipe G. 
+# Date:     15/Apr/2019
+# Version:  2
+
+#!/usr/bin/env bash 
+set -ea
+
+DRYRUN=false
+export RD_COLOR=0
+declare -a all_projects
+
+all_projects=( $(rd projects  list -% %name) )
+
+for project_name in "${all_projects[@]-}"
+do
+   	 if [[ "${DRYRUN:-}" == true ]]
+    	 then
+       	 	printf "DRYRUN: rd projects archives export -f %s.jar -p %s \n" "$project_name" "$project_name"
+	 else
+		rd projects configure get -p $project_name > $project_name.properties
+		sed -i "s/project.disable.schedule: true/project.disable.schedule: false/g" "$project_name".properties
+		rd projects configure set -p $project_name -f "$project_name".properties
+		printf "INFO: Enabling scheduled for project %s\n" "$project_name"
+		rm $project_name.properties
+	fi
+done
+echo "INFO: Done."
+exit $?
